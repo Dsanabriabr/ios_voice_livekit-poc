c010a4fb27d9143084ed3689faa172a540c582f1
Daniel Sanabria <sanabriabrasil@hotmail.com>
2026-06-10 19:25:01 -0300

    DEV - Create new CustomTabBar2.swift

---
+++ Helpers/CustomTabBar2.swift
@@ -0,0 +1,59 @@
+//
+//  CustomTabBar2.swift
+//  voice_to_text
+//
+//  Created by Daniel Sanabria on 08/06/26.
+//
+
+import SwiftUI
+
+struct CustomTabBar2 : UIViewRepresentable {
+    var size: CGSize
+    var barTint: Color = .gray.opacity(0.15)
+    @Binding var activeTab:CustomTab
+    
+    func updateUIView(_ uiView: UISegmentedControl, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    func makeUIView(context: Context) -> UISegmentedControl {
+        let items = CustomTab.allCases.compactMap({ _ in ""})
+        let control = UISegmentedControl(items: items)
+        control.selectedSegmentIndex = 0
+        
+        DispatchQueue.main.async {
+            for subview in control.subviews {
+                if subview is UIImageView && subview != control.subviews.last {
+                    subview.alpha = 0
+                }
+            }
+        }
+        control.selectedSegmentTintColor = UIColor(barTint)
+        control.addTarget(context.coordinator, action: #selector(context.coordinator.tabSelected(_:)), for: .valueChanged)
+        return control
+    }
+    
+    func sizeThatFits(_ proposal: ProposedViewSize, uiView: UISegmentedControl, context: Context) -> CGSize? {
+        return size
+    }
+    
+    class Coordinator: NSObject {
+        var parent: CustomTabBar2
+        init(parent: CustomTabBar2) {
+            self.parent = parent
+        }
+        
+        @objc func tabSelected(_ control: UISegmentedControl) {
+            parent.activeTab = CustomTab.allCases[control.selectedSegmentIndex]
+        }
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}

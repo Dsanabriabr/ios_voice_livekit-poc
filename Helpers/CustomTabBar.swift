86288d2a6785073450eec37c4c198898f2e16945
Daniel Sanabria <sanabriabrasil@hotmail.com>
2026-06-10 19:24:46 -0300

    DEV - Edit new CustomTabBar.swift

--- Helpers/CustomTabBar.swift
+++ Helpers/CustomTabBar.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct CustomTabBar<TabItemView: View> : UIViewRepresentable {
     var size: CGSize
-    var activeTint: Color = .blue
+    var activeTint: Color = .teal
+    var inactiveTint: Color = .gray.opacity(0.95)
     var barTint: Color = .gray.opacity(0.15)
     @Binding var activeTab:CustomTab
     @ViewBuilder var tabItemView: (CustomTab) -> TabItemView
@@ -26,8 +27,30 @@ struct CustomTabBar<TabItemView: View> : UIViewRepresentable {
         let items = CustomTab.allCases.map(\.rawValue)
         let control = UISegmentedControl(items: items)
         control.selectedSegmentIndex = 0
+        
+        for (index, tab) in CustomTab.allCases.enumerated() {
+            let renderer = ImageRenderer(content: tabItemView(tab))
+            renderer.scale = 2
+            let image = renderer.uiImage
+            control.setImage(image, forSegmentAt: index)
+        }
+        
+        DispatchQueue.main.async {
+            for subview in control.subviews {
+                if subview is UIImageView && subview != control.subviews.last {
+                    subview.alpha = 0
+                }
+            }
+        }
+        control.selectedSegmentTintColor = UIColor(barTint)
+        control.setTitleTextAttributes([.foregroundColor: UIColor(activeTint)], for: .selected)
+        control.setTitleTextAttributes([.foregroundColor: UIColor(inactiveTint)], for: .normal)
         control.addTarget(context.coordinator, action: #selector(context.coordinator.tabSelected(_:)), for: .valueChanged)
         return control
+    }
+    
+    func sizeThatFits(_ proposal: ProposedViewSize, uiView: UISegmentedControl, context: Context) -> CGSize? {
+        return size
     }
     
     class Coordinator: NSObject {
